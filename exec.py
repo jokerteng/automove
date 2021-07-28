195e0bb78185c10316c2c2ee03c29d199db83c8d
Joker Teng <jokerteng@hotmail.com>
2021-07-28 21:04:03 +0800

    update

--- exec.py
+++ exec.py
@@ -4,20 +4,13 @@ import shutil
 path = "."
 string = ".jpg.repair"
 
-temp_folder_name = "temp"
-temp_folder_path = path + os.sep + temp_folder_name
-try:
-    os.mkdir(temp_folder_name)
-    print("Create Done.")
-except FileExistsError:
-    print("Folder Exists.")
-
-file_path_list = [shutil.move(path + os.sep + file_name, temp_folder_path)
-                  for file_name in os.listdir(path) if string in file_name]
-[os.rename(file_name, file_name.replace(string, "")) for file_name in file_path_list]
-[os.remove(file_name) for file_name in os.listdir(path) if "jpg" in file_name]
-file_path_list = [shutil.move(temp_folder_path + os.sep + file_path, path) for file_path in
-                  os.listdir(temp_folder_path)]
-os.rmdir("./temp")
-
-# [shutil.move(file_path, path) for file_path in file_path_list]
+file_list = [file_name for file_name in os.listdir(path) if string in file_name]
+file_list = file_list + ["Thumbs.db"]
+for file_name in os.listdir(path):
+    file_path = path + os.sep + file_name
+    if os.path.isfile(file_path):
+        if file_name not in file_list:
+            os.remove(file_path)
+[os.rename(file_name, file_name.replace(string, "")) for file_name in
+        [path + os.sep + file_name for file_name in file_list]]
+print("Done.")
